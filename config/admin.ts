161ec522541a3ddbe86f080817c9203bc6ac205c
roy-dev-rb <roy@roundbytes.com>
2025-11-04 11:09:46 +0700

    updated strapi

--- config/admin.ts
+++ config/admin.ts
@@ -1,10 +1,6 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
-    sessions: {
-      maxSessionLifespan: env.int('ADMIN_SESSION_LIFESPAN', 60 * 60 * 24 * 30), // 30 days in seconds
-      maxRefreshTokenLifespan: env.int('ADMIN_REFRESH_TOKEN_LIFESPAN', 60 * 60 * 24 * 90), // 90 days in seconds
-    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -13,9 +9,6 @@ export default ({ env }) => ({
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
     },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
