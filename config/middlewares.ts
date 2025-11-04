2011dcf75a0d4ca50be6e39c0bd55f9a9f23d0b6
roy-dev-rb <roy@roundbytes.com>
2025-11-04 13:53:02 +0700

    ADD AWS S3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,34 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+            env('AWS_CDN_URL'),
+          ].filter(Boolean),
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+            env('AWS_CDN_URL'),
+          ].filter(Boolean),
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,39 @@
-export default ({ env }) => ({
-    'users-permissions': {
-        config: {
-            jwtSecret: env('JWT_SECRET'),
-        },
-    },
-    upload: {
-        config: {
-            provider: 'cloudinary',
-            providerOptions: {
-                cloud_name: env('CLOUDINARY_NAME'),
-                api_key: env('CLOUDINARY_KEY'),
-                api_secret: env('CLOUDINARY_SECRET'),
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
+export default ({ env }) => {
+    const config: any = {
+        'users-permissions': {
+            config: {
+                jwtSecret: env('JWT_SECRET'),
             },
         },
-    },
-});
+    };
+
+    // Use AWS S3 if bucket name is provided, otherwise use local storage
+    if (env('AWS_BUCKET')) {
+        config.upload = {
+            config: {
+                provider: 'aws-s3',
+                providerOptions: {
+                    baseUrl: env('AWS_CDN_URL'),
+                    s3Options: {
+                      credentials: {
+                        accessKeyId: env('AWS_ACCESS_KEY_ID', ''),
+                        secretAccessKey: env('AWS_ACCESS_SECRET', ''),
+                      },
+                      params: {
+                        ACL: env('AWS_ACL', 'public-read'),
+                        Bucket: env('AWS_BUCKET'),
+                      },
+                    },
+                  },
+                actionOptions: {
+                    upload: {},
+                    uploadStream: {},
+                    delete: {},
+                },
+            },
+        };
+    }
+    // If no AWS_S3_BUCKET, Strapi will use local storage by default
+
+    return config;
+};
