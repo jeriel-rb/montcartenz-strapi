c95f627cdf5a3b2b60421b3bc65be3bb99e7184d
roy-dev-rb <roy@roundbytes.com>
2025-11-06 11:35:53 +0700

    s3 config

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,33 +7,29 @@ export default ({ env }) => {
         },
     };
 
-    // Use AWS S3 if bucket name is provided, otherwise use local storage
-    if (env('AWS_BUCKET')) {
-        config.upload = {
-            config: {
-                provider: 'aws-s3',
-                providerOptions: {
-                    baseUrl: env('AWS_CDN_URL'),
-                    s3Options: {
-                      credentials: {
-                        accessKeyId: env('AWS_ACCESS_KEY_ID', ''),
-                        secretAccessKey: env('AWS_ACCESS_SECRET', ''),
-                      },
-                      params: {
-                        ACL: env('AWS_ACL', 'public-read'),
-                        Bucket: env('AWS_BUCKET'),
-                      },
-                    },
+    config.upload = {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                baseUrl: env('AWS_CDN_URL'),
+                s3Options: {
+                  credentials: {
+                    accessKeyId: env('AWS_ACCESS_KEY_ID', ''),
+                    secretAccessKey: env('AWS_ACCESS_SECRET', ''),
                   },
-                actionOptions: {
-                    upload: {},
-                    uploadStream: {},
-                    delete: {},
+                  params: {
+                    ACL: env('AWS_ACL', 'public-read'),
+                    Bucket: env('AWS_BUCKET'),
+                  },
                 },
+              },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
             },
-        };
-    }
-    // If no AWS_S3_BUCKET, Strapi will use local storage by default
+        },
+    };
 
     return config;
 };
